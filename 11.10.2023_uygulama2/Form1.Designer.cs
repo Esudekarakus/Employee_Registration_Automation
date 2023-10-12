5cff1c2cabcd9fd55bafd6e534aaf5941c843ac7
sude <sude@DESKTOP-FCGTNB5>
2023-10-12 08:53:26 +0300

    Add project files.

---
+++ 11.10.2023_uygulama2/Form1.Designer.cs
@@ -0,0 +1,420 @@
+﻿namespace _11._10._2023_uygulama2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            btnSil = new Button();
+            btnGuncelle = new Button();
+            btnKaydet = new Button();
+            btnResim = new Button();
+            pictureBox1 = new PictureBox();
+            dateTimeIs = new DateTimePicker();
+            label11 = new Label();
+            txtAdres = new TextBox();
+            label10 = new Label();
+            dateTimeDogumtarihi = new DateTimePicker();
+            txtMail = new TextBox();
+            txtTelefon = new TextBox();
+            txtSoyIsim = new TextBox();
+            txtIsim = new TextBox();
+            txtID = new TextBox();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            listView1 = new ListView();
+            columnHeader1 = new ColumnHeader();
+            Ad = new ColumnHeader();
+            Soyisim = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(35, 30);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(701, 315);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Personel Bilgileri";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(25, 115);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 0;
+            label3.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(25, 87);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 0;
+            label2.Text = "label1";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(25, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btnSil);
+            groupBox2.Controls.Add(btnGuncelle);
+            groupBox2.Controls.Add(btnKaydet);
+            groupBox2.Controls.Add(btnResim);
+            groupBox2.Controls.Add(pictureBox1);
+            groupBox2.Controls.Add(dateTimeIs);
+            groupBox2.Controls.Add(label11);
+            groupBox2.Controls.Add(txtAdres);
+            groupBox2.Controls.Add(label10);
+            groupBox2.Controls.Add(dateTimeDogumtarihi);
+            groupBox2.Controls.Add(txtMail);
+            groupBox2.Controls.Add(txtTelefon);
+            groupBox2.Controls.Add(txtSoyIsim);
+            groupBox2.Controls.Add(txtIsim);
+            groupBox2.Controls.Add(txtID);
+            groupBox2.Controls.Add(label9);
+            groupBox2.Controls.Add(label8);
+            groupBox2.Controls.Add(label7);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Location = new Point(29, 30);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(701, 315);
+            groupBox2.TabIndex = 0;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Personel Bilgileri";
+            // 
+            // btnSil
+            // 
+            btnSil.Location = new Point(583, 284);
+            btnSil.Name = "btnSil";
+            btnSil.Size = new Size(75, 23);
+            btnSil.TabIndex = 9;
+            btnSil.Text = "Sil";
+            btnSil.UseVisualStyleBackColor = true;
+            btnSil.Click += btnSil_Click;
+            // 
+            // btnGuncelle
+            // 
+            btnGuncelle.Location = new Point(583, 255);
+            btnGuncelle.Name = "btnGuncelle";
+            btnGuncelle.Size = new Size(75, 23);
+            btnGuncelle.TabIndex = 8;
+            btnGuncelle.Text = "Güncelle";
+            btnGuncelle.UseVisualStyleBackColor = true;
+            btnGuncelle.Click += btnGuncelle_Click;
+            // 
+            // btnKaydet
+            // 
+            btnKaydet.Location = new Point(583, 226);
+            btnKaydet.Name = "btnKaydet";
+            btnKaydet.Size = new Size(75, 23);
+            btnKaydet.TabIndex = 8;
+            btnKaydet.Text = "Kaydet";
+            btnKaydet.UseVisualStyleBackColor = true;
+            btnKaydet.Click += btnKaydet_Click;
+            // 
+            // btnResim
+            // 
+            btnResim.Location = new Point(583, 197);
+            btnResim.Name = "btnResim";
+            btnResim.Size = new Size(75, 23);
+            btnResim.TabIndex = 8;
+            btnResim.Text = "Resim Seç";
+            btnResim.UseVisualStyleBackColor = true;
+            btnResim.Click += btnResim_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(352, 197);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 108);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            // 
+            // dateTimeIs
+            // 
+            dateTimeIs.Location = new Point(458, 144);
+            dateTimeIs.Name = "dateTimeIs";
+            dateTimeIs.Size = new Size(200, 23);
+            dateTimeIs.TabIndex = 6;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(374, 150);
+            label11.Name = "label11";
+            label11.Size = new Size(78, 15);
+            label11.TabIndex = 5;
+            label11.Text = "İşe Giriş Tarihi";
+            // 
+            // txtAdres
+            // 
+            txtAdres.Location = new Point(458, 55);
+            txtAdres.Multiline = true;
+            txtAdres.Name = "txtAdres";
+            txtAdres.Size = new Size(200, 71);
+            txtAdres.TabIndex = 4;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(374, 58);
+            label10.Name = "label10";
+            label10.Size = new Size(37, 15);
+            label10.TabIndex = 3;
+            label10.Text = "Adres";
+            // 
+            // dateTimeDogumtarihi
+            // 
+            dateTimeDogumtarihi.Location = new Point(109, 142);
+            dateTimeDogumtarihi.Name = "dateTimeDogumtarihi";
+            dateTimeDogumtarihi.Size = new Size(200, 23);
+            dateTimeDogumtarihi.TabIndex = 2;
+            // 
+            // txtMail
+            // 
+            txtMail.Location = new Point(109, 198);
+            txtMail.Name = "txtMail";
+            txtMail.Size = new Size(200, 23);
+            txtMail.TabIndex = 1;
+            // 
+            // txtTelefon
+            // 
+            txtTelefon.Location = new Point(109, 170);
+            txtTelefon.Name = "txtTelefon";
+            txtTelefon.Size = new Size(200, 23);
+            txtTelefon.TabIndex = 1;
+            // 
+            // txtSoyIsim
+            // 
+            txtSoyIsim.Location = new Point(109, 115);
+            txtSoyIsim.Name = "txtSoyIsim";
+            txtSoyIsim.Size = new Size(200, 23);
+            txtSoyIsim.TabIndex = 1;
+            // 
+            // txtIsim
+            // 
+            txtIsim.Location = new Point(109, 86);
+            txtIsim.Name = "txtIsim";
+            txtIsim.Size = new Size(200, 23);
+            txtIsim.TabIndex = 1;
+            // 
+            // txtID
+            // 
+            txtID.Location = new Point(109, 58);
+            txtID.Name = "txtID";
+            txtID.Size = new Size(200, 23);
+            txtID.TabIndex = 1;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(25, 201);
+            label9.Name = "label9";
+            label9.Size = new Size(41, 15);
+            label9.TabIndex = 0;
+            label9.Text = "E-Mail";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(25, 170);
+            label8.Name = "label8";
+            label8.Size = new Size(45, 15);
+            label8.TabIndex = 0;
+            label8.Text = "Telefon";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(25, 142);
+            label7.Name = "label7";
+            label7.Size = new Size(78, 15);
+            label7.TabIndex = 0;
+            label7.Text = "Doğum Tarihi";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(25, 114);
+            label4.Name = "label4";
+            label4.Size = new Size(48, 15);
+            label4.TabIndex = 0;
+            label4.Text = "Soyisim";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(25, 86);
+            label5.Name = "label5";
+            label5.Size = new Size(29, 15);
+            label5.TabIndex = 0;
+            label5.Text = "İsim";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(25, 58);
+            label6.Name = "label6";
+            label6.Size = new Size(66, 15);
+            label6.TabIndex = 0;
+            label6.Text = "Personel ID";
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, Ad, Soyisim, columnHeader3, columnHeader2, columnHeader4, columnHeader5 });
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+            listView1.Location = new Point(29, 375);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(707, 157);
+            listView1.TabIndex = 1;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Personel ID";
+            columnHeader1.Width = 100;
+            // 
+            // Ad
+            // 
+            Ad.Text = "Ad";
+            Ad.Width = 100;
+            // 
+            // Soyisim
+            // 
+            Soyisim.Text = "Soyisim";
+            Soyisim.Width = 130;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Doğum Tarihi";
+            columnHeader3.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Telefon";
+            columnHeader2.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "E-Mail";
+            columnHeader4.Width = 100;
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.Text = "Resim Yolu";
+            columnHeader5.Width = 100;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 544);
+            Controls.Add(listView1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private GroupBox groupBox2;
+        private Button btnSil;
+        private Button btnGuncelle;
+        private Button btnKaydet;
+        private Button btnResim;
+        private PictureBox pictureBox1;
+        private DateTimePicker dateTimeIs;
+        private Label label11;
+        private TextBox txtAdres;
+        private Label label10;
+        private DateTimePicker dateTimeDogumtarihi;
+        private TextBox txtMail;
+        private TextBox txtTelefon;
+        private TextBox txtSoyIsim;
+        private TextBox txtIsim;
+        private TextBox txtID;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader Ad;
+        private ColumnHeader Soyisim;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+    }
+}
---
+++ 11.10.2023_uygulama2/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System.Security.Cryptography;
+
+namespace _11._10._2023_uygulama2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnGuncelle.Enabled = false;
+            btnSil.Enabled = false;
+        }
+        Person kisi = new Person();
+        List<Person> list = new List<Person>();
+
+        private void btnResim_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.Filter = "Jpg (.jpg)|*.jpg|Png (.png)|*.png";
+            ofd.ShowDialog();
+            string dosyaYolu = ofd.FileName;
+            pictureBox1.ImageLocation = dosyaYolu;
+            kisi.ResimYolu = dosyaYolu;
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            kisi.Ad = txtIsim.Text;
+            kisi.Soyad = txtSoyIsim.Text;
+            kisi.Email = txtMail.Text;
+            kisi.PersonelID = txtID.Text;
+            kisi.DogumTarihi = dateTimeDogumtarihi.Value;
+            kisi.Telefon = txtTelefon.Text;
+            kisi.Adres = txtAdres.Text;
+            kisi.IseGirisTarihi = dateTimeIs.Value;
+
+
+            int yas = (int)DateTime.Now.Year - kisi.DogumTarihi.Year;
+            int result = DateTime.Compare(DateTime.Now, kisi.IseGirisTarihi);
+            //EĞER result 0 dan küçükse işe giriş tarihi şuanki zamandan öncedir, 0 a eşitse aynı zamanı ifade der, 0 dan büyükse daha geç
+
+            if (kisi.PersonelID.Length >= 5 && kisi.Email.Contains("@hotmail.com") && yas >= 18 && result >= 0)
+            {
+                btnGuncelle.Enabled = true;
+                btnSil.Enabled = true;
+                list.Add(kisi);
+                ListViewDoldur();
+                MessageBox.Show("Kayıt başarılı");
+                txtAdres.Clear();
+                txtID.Clear();
+                txtIsim.Clear();
+                txtMail.Clear();
+                txtSoyIsim.Clear();
+                txtTelefon.Clear();
+                pictureBox1.Image = null;
+                
+            }
+            //else if (txtAdres.Text== " " || txtID.Text==" " || txtIsim.Text=" ")
+            else
+            {
+                MessageBox.Show("Hatalı giriş yaptınız, lütfen tekrar deneyiniz");
+            }
+
+
+
+
+        }
+        private void ListViewDoldur()
+        {
+            listView1.Items.Clear();
+            foreach (Person kisi in list)
+            {
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text = kisi.PersonelID;
+                lvi.SubItems.Add(kisi.Ad);
+                lvi.SubItems.Add(kisi.Soyad);
+                lvi.SubItems.Add(kisi.DogumTarihi.ToShortDateString());//sadece string kabul ediyor, int ları otomatşk strşnge dönüştürüyor ama datetime da yapamıyor
+                lvi.SubItems.Add(kisi.Telefon);
+                lvi.SubItems.Add(kisi.Email);
+                lvi.SubItems.Add(kisi.ResimYolu);
+
+                listView1.Items.Add(lvi);
+            }
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+
+            List<int> seciliIndexler = new List<int>();
+
+            foreach (int index in listView1.SelectedIndices)
+            {
+
+                ListViewItem selectedItem = listView1.Items[index];
+                string itemText = selectedItem.Text;
+
+
+
+                seciliIndexler.Add(index);
+
+            }
+            foreach (int index in seciliIndexler)
+            {
+                listView1.Items.RemoveAt(index);
+                list.RemoveAt(index);
+                kisi.Ad = txtIsim.Text;
+                kisi.Soyad = txtSoyIsim.Text;
+                kisi.Email = txtMail.Text;
+                kisi.PersonelID = txtID.Text;
+                kisi.DogumTarihi = dateTimeDogumtarihi.Value;
+                kisi.Telefon = txtTelefon.Text;
+                kisi.Adres = txtAdres.Text;
+                kisi.IseGirisTarihi = dateTimeIs.Value;
+                int yas = (int)DateTime.Now.Year - kisi.DogumTarihi.Year;
+                int result = DateTime.Compare(DateTime.Now, kisi.IseGirisTarihi);
+                if (kisi.PersonelID.Length >= 5 && kisi.Email.Contains("@hotmail.com") && yas >= 18 && result >= 0)
+                {
+                    btnGuncelle.Enabled = true;
+                    btnSil.Enabled = true;
+                    list.Add(kisi);
+                    ListViewDoldur();
+                    MessageBox.Show("Kayıt başarılı");
+                    txtAdres.Clear();
+                    txtID.Clear();
+                    txtIsim.Clear();
+                    txtMail.Clear();
+                    txtSoyIsim.Clear();
+                    txtTelefon.Clear();
+                    pictureBox1.Image = null;
+                }
+                
+                else
+                {
+                    MessageBox.Show("Hatalı giriş yaptınız, lütfen tekrar deneyiniz");
+                }
+
+
+            }
+
+
+
+
+
+
+
+
+
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            List<int> seciliIndexler = new List<int>();
+
+            foreach (int index in listView1.SelectedIndices)
+            {
+                
+                ListViewItem selectedItem = listView1.Items[index];
+                string itemText = selectedItem.Text;
+
+                
+                
+                    seciliIndexler.Add(index);
+                
+            }
+            foreach (int index in seciliIndexler)
+            {
+                listView1.Items.RemoveAt(index);
+                list.RemoveAt(index);
+
+            }
+        }
+    }
+}
